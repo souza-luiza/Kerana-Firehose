f3732b277fb1a15d48cc4a0878eb6910ca947f9a
Luiza de Souza <souza.luiza@unifesp.br>
2025-06-06 20:29:39 -0300

    sistema de vida e menu de missões.

---
+++ Assets/HealthUI.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    public Image heartPrefab;
+    public Sprite fullHeartSprite;
+    public Sprite emptyHeartSprite;
+
+    private List<Image> hearts = new List<Image>();
+
+    public void SetMaxHearts(int maxHearts)
+    {
+        foreach (Image heart in hearts)
+        {
+            Destroy(heart.gameObject);
+        }
+
+        hearts.Clear();
+
+        for (int i = 0; i < maxHearts; i++)
+        {
+            Image newHeart = Instantiate(heartPrefab, transform);
+            newHeart.sprite = fullHeartSprite;
+            newHeart.color = heartPrefab.color; //coração vermelho
+            hearts.Add(newHeart);
+        }
+    }
+
+    public void UpdateHearts(int currentHealth)
+    {
+        for (int i = 0; i < hearts.Count; i++)
+        {
+            if (i < currentHealth)
+            {
+                hearts[i].sprite = fullHeartSprite;
+                hearts[i].color = heartPrefab.color; //coração cheio
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeartSprite;
+                hearts[i].color = Color.black; //talvez black
+            }
+        }
+    }
+}
---
+++ Assets/QuestUI.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QuestUI : MonoBehaviour
+{
+    public Transform questListContent;
+    public GameObject questEntryPrefab;
+    public GameObject objectiveTextPrefab;
+
+    public Quest testQuest;
+    public int testQuestAmount;
+    private List<QuestProgress> testQuests = new();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < testQuestAmount; i++)
+        {
+            testQuests.Add(new QuestProgress(testQuest));
+        }
+
+        UpdateQuestUI();
+    }
+
+    public void UpdateQuestUI()
+    {
+        //Destroy existing quest entries
+        foreach (Transform child in questListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        //Build quest entries
+        foreach (var quest in testQuests)
+        {
+            GameObject entry = Instantiate(questEntryPrefab, questListContent);
+            TMP_Text questNameText = entry.transform.Find("QuestNameText").GetComponent<TMP_Text>();
+            Transform objectiveList = entry.transform.Find("ObjectiveList");
+
+            questNameText.text = quest.quest.name;
+
+            foreach (var objective in quest.objectives)
+            {
+                GameObject objTextGO = Instantiate(objectiveTextPrefab, objectiveList);
+                TMP_Text objText = objTextGO.GetComponent<TMP_Text>();
+                objText.text = $"{objective.description} ({objective.currentAmount}/{objective.requiredAmount})"; //Mate 3 moscas (0/3)
+            }
+        }
+    }
+}
