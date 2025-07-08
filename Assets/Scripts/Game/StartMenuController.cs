549d18ed5fb206fee47a170dec66738ed3d63ed5
Luiza de Souza <souza.luiza@unifesp.br>
2025-07-08 00:01:49 -0300

    Adição de créditos e alterações na UI

--- Assets/Scripts/Game/StartMenuController.cs
+++ Assets/Scripts/Game/StartMenuController.cs
@@ -3,9 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class StartMenuController : MonoBehaviour
 {
+    public GameObject startButton;
+    public GameObject creditsButton;
+    public GameObject exitButton;
+    public GameObject creditsText;
+    public GameObject returnButton;
+
     public void OnStartClick()
     {
         SceneManager.LoadScene("Fase1_Melhorada");
+    }
+
+    public void OnCreditsEnterClick()
+    {
+        startButton.SetActive(false);
+        creditsButton.SetActive(false);
+        exitButton.SetActive(false);
+        creditsText.SetActive(true);
+        returnButton.SetActive(true);
+    }
+
+    public void OnCreditsExitClick()
+    {
+        creditsText.SetActive(false);
+        returnButton.SetActive(false);
+        startButton.SetActive(true);
+        creditsButton.SetActive(true);
+        exitButton.SetActive(true);
     }
 
     public void OnExitClick()
