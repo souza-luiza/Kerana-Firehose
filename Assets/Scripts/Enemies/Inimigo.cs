cae29a1820c3dad3e9abed9de7d567d809c0e030
Luiza de Souza <souza.luiza@unifesp.br>
2025-07-09 04:40:20 -0300

    Adição de Musgos de outras cores
    Adição de sinalização de dano nos musgos também.

--- Assets/Scripts/Enemies/Inimigo.cs
+++ Assets/Scripts/Enemies/Inimigo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Inimigo : MonoBehaviour
@@ -14,10 +15,18 @@ public class Inimigo : MonoBehaviour
     public string ID;
     public int damage = 1;
 
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     public void ReceberDano(int dano)
     {
-        vidas-=dano;
+        vidas -= dano;
         Debug.Log(name + " recebeu dano. Vida: " + vidas);
+
         if (vidas <= 0)
         {
             //Derrotado
@@ -33,10 +42,21 @@ public class Inimigo : MonoBehaviour
             //Recebeu dano
             animator.SetTrigger("Dano");
         }
+        else
+        {
+            StartCoroutine(FlashRed());
+        }
     }
 
     public void SetOnDeathCallback(Action callback)
     {
         onDeathCallback = callback;
+    }
+    
+    private IEnumerator FlashRed()
+    {
+        spriteRenderer.color = new Color(0.9333333f, 0.3294118f, 0.3294118f, 1f); //cor para dano
+        yield return new WaitForSeconds(0.2f);
+        spriteRenderer.color = Color.white;
     }
 }
